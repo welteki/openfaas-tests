51a30cd13a668b51b20c2a40a79cfebbefc768da
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2022-07-17 00:18:02 +0200

    Use sudo for ctr command

--- faasd/213-scale-up-health/test.sh
+++ faasd/213-scale-up-health/test.sh
@@ -7,7 +7,7 @@ echo Attempting scale up event ${N} times...
 
 for ((i=1; i<=$N; i++ ));
 do
- ctr -n openfaas-fn task rm figlet -f > /dev/null 2>&1
+ sudo ctr -n openfaas-fn task rm figlet -f > /dev/null 2>&1
  sleep 1
  k6 run -q --vus 30 --duration 15s ${script_dir}/script.js -o output-prometheus-remote
 done
